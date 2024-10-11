00624388c645028c801cf1092ffc57ab8f4c09bb
eugene-hut <eugene.hut8@gmail.com>
2024-10-11 12:02:30 +0300

    fix

--- excel/api/v1/handles.py
+++ excel/api/v1/handles.py
@@ -25,6 +25,8 @@ async def replace_photo(
         sheet = wb.active
         for row in sheet.rows:
             for cell in row:
+                if cell not isistance(cell.value, str):
+                    continue
                 cell_value = cell.value.split(";")
                 new_value = cell_value[0] + data.text
                 if cell_value[-1].endswith(".mp4"):
